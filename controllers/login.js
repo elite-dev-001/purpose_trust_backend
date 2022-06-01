b6e35627c5140ce10370f4e92c0e173f25868166
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-06-01 09:14:43 +0100

    cors

--- controllers/login.js
+++ controllers/login.js
@@ -43,7 +43,16 @@ const loginControl = async (req, res) => {
             },
             JWT_SECRET
             )
-            return res.json({ status: "ok", data: token, role: 'admin'})
+            return {
+                statusCode: 200,
+                 headers: {
+              "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+              "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+              "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+              "Access-Control-Allow-Methods": "POST, OPTIONS"
+            },
+            body: JSON.stringify(item)
+            };
         } else {
             return res.json({ status: 'error', error: 'Incorrect password'})
         }
