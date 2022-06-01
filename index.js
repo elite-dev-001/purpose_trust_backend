e9a2227a8ed5e9557bca2bcefd2aed42013ecdba
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-06-01 13:26:39 +0100

    local

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 this.app.use(function (req, res, next) {
     res.header("Access-Control-Alllow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Private-Network", true)
+    // res.header("Access-Control-Allow-Private-Network", true)
     if(req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET')
         return res.status(200).json({})
