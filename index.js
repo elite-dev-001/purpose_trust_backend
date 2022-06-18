b5a98ca5edf2370bb6a29de7b2171a8e294eb9d2
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-06-18 20:02:54 +0100

    finally

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let port = process.env.PORT || 5000;
 const app = express(); 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Alllow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Private-Network", true)
     if(req.method === "OPTIONS") {
